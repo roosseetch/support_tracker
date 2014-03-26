5b7427c233e71f34e3eb95b42036baa293b735df
Rustem <rustem_ss@mail.ru>
2014-03-26 13:49:41 +0200

    Track table row like a link on Staff Page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,11 +19,32 @@
 $(document).on('page:load', function () {
         $('a[href="' + this.location.pathname + '"]').parent().addClass('active');
         // root 'ip_addresses#index'
-        if (this.location.pathname == '/ip_addresses') {
+        if (this.location.pathname == '/tickets') {
           $('a[href="/"]').parent().addClass('active');
         }
 });
 
-$(document).on('page:load', function () {
-        $('.page.current').parent().addClass('active');
-});
+// $(document).on('page:load', function () {
+//         $('.page.current').parent().addClass('active');
+// });
+
+// $('tr[data-link]').on('click', function() {
+//   // window.location = this.data("link")
+//   window.document.location = $(this).attr('data-link');
+// });
+
+// jQuery(document).on('page:load', function($) {
+//       $("tr[href]").click(function() {
+//             window.document.location = $(this).attr("href");
+//       });
+// });
+
+// jQuery(document).on('page:load', function($) {
+//       $('tr[data-link]').click(function() {
+//             window.document.location = $(this).attr('data-link');
+//       });
+// });
+
+// $('tr[data-link]').on('click', function() {
+//     window.location = $(this).find('a').attr('href');
+// });
